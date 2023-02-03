13f21c7385840131d4a5eaf3010ed8506261ba5b
Dbitler <dbitler@hawk.iit.edu>
2023-02-03 14:16:50 -0600

    Initial Commit

---
+++ Monte-Carlo/Monte_CarloApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monte_CarloApp.swift
+//  Monte-Carlo
+//
+//  Created by IIT PHYS 440 on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Monte_CarloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
